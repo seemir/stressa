3fac92b137d6d1d59e73c47bc5ee408360fccee7
seemir <samir.adrik@gmail.com>
2019-07-22 23:36:24 +0200

    First POC of sifo api successful

--- sifo.py
+++ sifo.py
@@ -3,11 +3,19 @@
 __author__ = 'Samir Adrik'
 __email__ = 'samir.adrik@gmail.com'
 
-import mechanize
+from mechanize import Browser
+from bs4 import BeautifulSoup
 
-br = mechanize.Browser()
-br.set_handle_robots(False)
-br.set_handle_refresh(False)
-br.open('http://kalkulator.referansebudsjett.no/php/blank_template.php')
-br.select_form(name="budsjett")
+browser = Browser()
+browser.set_handle_robots(False)
+browser.set_handle_refresh(False)
 
+browser.open('http://kalkulator.referansebudsjett.no/php/blank_template.php')
+browser.select_form('budsjett')
+browser["kjonn0"] = ['m']
+browser["alder0"] = ['50']
+browser["inntekt"] = '350000'
+br_response = browser.submit()
+
+soup = BeautifulSoup(br_response, "html.parser")
+print(soup.prettify())
