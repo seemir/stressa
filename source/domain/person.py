382f878fa8e6e007717a745fc255d48f9024f86d
seemir <samir.adrik@gmail.com>
2019-07-25 23:14:31 +0200

    Restructed repository

---
+++ source/domain/person.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+__author__ = 'Samir Adrik'
+__email__ = 'samir.adrik@gmail.com'
+
+
+class Person:
+
+    def __init__(self, sex='0', age='0', kinder_garden='0', sfo='0'):
+        self.sex = sex
+        self.age = age
+        self.pregnant = pregnant
+        self.kinder_garden = kinder_garden
+        self.sfo = sfo
+
+
