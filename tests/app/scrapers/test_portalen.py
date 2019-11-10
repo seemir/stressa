59b9862cb7a82576f21d63e8db3246124bef6928
seemir <samir.adrik@gmail.com>
2019-11-10 13:42:29 +0100

    Fixing problem with failing build (60d82cb2237af1dfe3d8980adc03d72376f46591)

--- tests/app/scrapers/test_portalen.py
+++ tests/app/scrapers/test_portalen.py
@@ -1,4 +1,9 @@
 # -*- coding: utf-8 -*-
 
+"""
+Test module for Portalen scraper against Finansportalen.no grunndata for boliglån
+
+"""
+
 __author__ = 'Samir Adrik'
 __email__ = 'samir.adrik@gmail.com'
