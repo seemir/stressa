a752fcee04fcca2dd1453635c9ef9f01c0c0504f
seemir <samir.adrik@gmail.com>
2019-12-01 20:47:51 +0100

    Auto stash before merge of "master" and "origin/master"

--- source/ui/models/settings.py
+++ source/ui/models/settings.py
@@ -1,31 +1,7 @@
 # -*- coding: utf-8 -*-
 
-"""
-Settings file for views
-
-"""
-
 __author__ = 'Samir Adrik'
 __email__ = 'samir.adrik@gmail.com'
-
-FINN_URL = 'https://www.finn.no/realestate/browse.html'
-
-FIELDS = {"kjønn": ["",
-                    "Mann",
-                    "Kvinne"],
-          "lånetype": ["",
-                       "Sammenligning",
-                       "Annuitetslån",
-                       "Serielån"],
-          "låneperiode": [""] +
-                         [str(yr) + " år" for yr in range(1, 31)],
-          "interval": ["",
-                       "Ukentlig",
-                       "Annenhver uke",
-                       "Månedlig",
-                       "Annenhver måned",
-                       "Kvartalsvis",
-                       "Årlig"]}
 
 LINE_EDITS = {"postnr": ["postnr",
                          "poststed",
@@ -51,4 +27,25 @@ LINE_EDITS = {"postnr": ["postnr",
                             "primrrom",
                             "bruttoareal",
                             "energimerking",
-                            "tomteareal"]}
+                            "tomteareal"],
+              "navn": ["fornavn",
+                       "etternavn"]}
+
+FIELDS = {"kjønn": ["",
+                    "Mann",
+                    "Kvinne"],
+          "lånetype": ["",
+                       "Sammenligning",
+                       "Annuitetslån",
+                       "Serielån"],
+          "låneperiode": [""] +
+                         [str(yr) + " år" for yr in range(1, 31)],
+          "interval": ["",
+                       "Ukentlig",
+                       "Annenhver uke",
+                       "Månedlig",
+                       "Annenhver måned",
+                       "Kvartalsvis",
+                       "Årlig"]}
+
+FINN_URL = 'https://www.finn.no/realestate/browse.html'
