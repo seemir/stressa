522145a7c4cbd1c5a6b23c9813e172eb05b2615b
seemir <samir.adrik@gmail.com>
2020-01-02 23:26:27 +0100

    build error

--- source/ui/views/home_view.py
+++ source/ui/views/home_view.py
@@ -4,7 +4,7 @@ __author__ = 'Samir Adrik'
 __email__ = 'samir.adrik@gmail.com'
 
 import os
-import ctypes
+from ctypes import windll
 
 from PyQt5.QtWidgets import QMainWindow
 from PyQt5.uic import loadUi
@@ -16,7 +16,7 @@ from .error_view import ErrorView
 from . import resources
 
 myappid = 'mycompany.myproduct.subproduct.version'  # arbitrary string
-ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(myappid)
+windll.shell32.SetCurrentProcessExplicitAppUserModelID(myappid)
 
 
 class HomeView(QMainWindow):
