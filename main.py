199cee0e0eff8235ecf42846d509e78b9597feaf
seemir <samir.adrik@gmail.com>
2020-01-30 00:52:24 +0100

    Added `AA_DisableHighDpiScaling`

--- main.py
+++ main.py
@@ -10,12 +10,14 @@ __email__ = 'samir.adrik@gmail.com'
 
 import sys
 
-from PyQt5.QtCore import QFile, QTextStream
+from PyQt5.QtCore import QFile, QTextStream, Qt
 from PyQt5.QtWidgets import QApplication
 
 from source.ui import HomeView, SplashView
 
 if __name__ == "__main__":
+    QApplication.setAttribute(Qt.AA_DisableHighDpiScaling, True)
+
     f = QFile("source/ui/default_theme.qss")
     f.open(QFile.ReadOnly | QFile.Text)
     ts = QTextStream(f)
